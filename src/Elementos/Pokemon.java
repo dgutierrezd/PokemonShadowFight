ad829f19c7d0f62a8216650ff9a556742252a763
Sebastian Cordero Ramirez <scordero414@gmail.com>
2019-02-20 17:59:30 -0500

    Actualizamos las clases abstractas con sus metodos correspondientes

--- src/Elementos/Pokemon.java
+++ src/Elementos/Pokemon.java
@@ -10,5 +10,47 @@ package Elementos;
  * @author USUARIO
  */
 public abstract class Pokemon {
+    public static final int ATAQUE = 1;
+    private int resistenciaVida;
+    private int probabilidadAtaque;
+    private int prbabilidadDefensa;
+    private String nombre;
+    private String tipo;
+
+    public Pokemon(int resistenciaVida, int probabilidadAtaque, int prbabilidadDefensa) {
+        this.resistenciaVida = resistenciaVida;
+        this.probabilidadAtaque = probabilidadAtaque;
+        this.prbabilidadDefensa = prbabilidadDefensa;
+    }
+    public abstract void atacar();
     
+    public abstract void defender();
+
+    public static int getATAQUE() {
+        return ATAQUE;
+    }
+
+    public int getResistenciaVida() {
+        return resistenciaVida;
+    }
+
+    public int getProbabilidadAtaque() {
+        return probabilidadAtaque;
+    }
+
+    public int getPrbabilidadDefensa() {
+        return prbabilidadDefensa;
+    }
+
+    public void setProbabilidadAtaque(int probabilidadAtaque) {
+        this.probabilidadAtaque = probabilidadAtaque;
+    }
+
+    public void setPrbabilidadDefensa(int prbabilidadDefensa) {
+        this.prbabilidadDefensa = prbabilidadDefensa;
+    }
+
+    public void setResistenciaVida(int resistenciaVida) {
+        this.resistenciaVida = resistenciaVida;
+    }
 }
